da38675f45ff6ea214d7e08680eba5828c5fb7db
zeyad.h.abaza <zeyad.h.abaza@gmail.com>
2025-01-07 19:42:20 +0200

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Author;
+use App\Models\Book;
+use App\Models\Category;
+use App\Observers\AuthorObserver;
+use App\Observers\BookObserver;
+use App\Observers\CategoryObserver;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Book::observe(BookObserver::class);
+        Category::observe(CategoryObserver::class);
+        Author::observe(AuthorObserver::class);
+    }
+}
