06bff5a747432a42b3a92b747660894bb6798053
ziad hassan <145067859+Ziad-Abaza@users.noreply.github.com>
2025-01-15 13:39:18 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Middleware to authenticate Sanctum requests
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/user', function (Request $request) {
+        $user = $request->user()->load('roles');
+        return $user;
+    });
 });
 
 /*
