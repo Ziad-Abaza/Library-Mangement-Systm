4f3b30f2ce7695bbd6a6ebaa048dd82b17c5ad9d
ziad hassan <145067859+Ziad-Abaza@users.noreply.github.com>
2025-01-18 20:51:39 +0200

    Update RoleUserSeeder.php

--- database/seeders/RoleUserSeeder.php
+++ database/seeders/RoleUserSeeder.php
@@ -11,13 +11,13 @@ class RoleUserSeeder extends Seeder
     public function run(): void
     {
         $user1 = User::find(1); // Assuming user with id 1 exists
-        $user2 = User::find(2); // Assuming user with id 2 exists
+        // $user2 = User::find(2); // Assuming user with id 2 exists
 
         $roleAdmin = Role::find(1); // Assuming role with id 1 exists (Admin)
-        $roleUser = Role::find(3);  // Assuming role with id 3 exists (User)
+        // $roleUser = Role::find(3);  // Assuming role with id 3 exists (User)
 
         $user1->roles()->attach($roleAdmin); // Assign Admin role to user 1
-        $user2->roles()->attach($roleUser);  // Assign User role to user 2
+        // $user2->roles()->attach($roleUser);  // Assign User role to user 2
 
     }
 }
